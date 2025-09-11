223c439b1a4fbbf03d7dcb406b0b72ff5d141637
MythEclipse <asepharyanalm001@gmail.com>
2025-09-11 17:36:07 +0700

    fix: Update deployment workflows for Next.js and Rust applications, including pnpm version, node version, and PM2 configuration files

---
+++ rust.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'RustExpress',
+      script: './target/release/rust',
+      exec_mode: 'fork',
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      error_file: 'logs/rust-error.log',
+      out_file: 'logs/rust-out.log',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4099,
+      },
+    },
+  ],
+};
